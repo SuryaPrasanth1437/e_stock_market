23c6c1df01dddc93b5cd49421c85cd0668ec8f50
unknown <suryaprasanth81@gmail.com>
2022-04-20 15:21:46 +0530

    microservice added

---
+++ Backend/stock-price/src/main/java/com/stock/market/stockprice/StockPriceApplication.java
@@ -0,0 +1,13 @@
+package com.stock.market.stockprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPriceApplication.class, args);
+	}
+
+}
