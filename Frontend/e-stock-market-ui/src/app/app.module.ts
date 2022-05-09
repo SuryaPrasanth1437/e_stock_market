86bf23666402dee029bd9b8b37b61cf2ee9f3c36
unknown <suryaprasanth81@gmail.com>
2022-05-09 16:30:23 +0530

    date picker added

--- Frontend/e-stock-market-ui/src/app/app.module.ts
+++ Frontend/e-stock-market-ui/src/app/app.module.ts
@@ -15,13 +15,24 @@ import { RegisterCompanyComponent } from './company-details/register-company/reg
 import { ViewCompanyDetailComponent } from './company-details/view-company-detail/view-company-detail.component';
 import { AddStockPriceComponent } from './stocks/add-stock-price/add-stock-price.component';
 import { ViewStocksComponent } from './stocks/view-stocks/view-stocks.component';
+import {MatFormFieldModule  } from '@angular/material/form-field';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatInputModule} from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxMatDatetimePickerModule, NgxMatNativeDateModule, NgxMatTimepickerModule } from '@angular-material-components/datetime-picker';
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', component: LoginComponent },
   { path: 'get-all-detail', component: CompanyDetailComponent },
   { path: 'register', component: RegisterCompanyComponent },
   { path: 'view-company-detail/:companyCode', component: ViewCompanyDetailComponent },
-  { path: 'add-stock-price/:companyCode', component: AddStockPriceComponent }
+  { path: 'add-stock-price/:companyCode', component: AddStockPriceComponent },
+  { path: 'search', component: ViewStocksComponent }
 ]
 @NgModule({
   declarations: [
@@ -44,6 +55,18 @@ export const routes: Routes = [
     RecaptchaFormsModule,
     RecaptchaModule,
     RecaptchaV3Module,
+    NgxMatDatetimePickerModule,
+    NgxMatTimepickerModule,
+    NgxMatNativeDateModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
     RouterModule.forRoot(routes)
   ],
   providers: [
--- Frontend/e-stock-market-ui/src/app/stocks/view-stocks/view-stocks.component.ts
+++ Frontend/e-stock-market-ui/src/app/stocks/view-stocks/view-stocks.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import * as moment from 'moment';
+import { ThemePalette } from '@angular/material/core';
+import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-view-stocks',
   templateUrl: './view-stocks.component.html',
   styleUrls: ['./view-stocks.component.css']
 })
 export class ViewStocksComponent implements OnInit {
+  "viewStockPrice": FormGroup
+  @ViewChild('picker') picker: any;
 
-  constructor() { }
+  public "date": moment.Moment;
+  public "disabled" = true;
+  public "showSpinners" = true;
+  public "showSeconds" = false;
+  public "touchUi" = true;
+  public "enableMeridian" = true;
+  public "minDate": moment.Moment;
+  public "maxDate"= new Date();
+  public "stepHour" = 1;
+  public "stepMinute" = 1;
+  public "stepSecond" = 1;
+  public "disableMinute"=false;
+public "hideTime"=false;
+  public color: ThemePalette = 'primary';
+  
+  constructor(private datePipe: DatePipe) { }
 
   ngOnInit(): void {
+    this.viewStockPrice = new FormGroup({
+      "search": new FormControl(null, [Validators.required]),
+      "endDate": new FormControl(null, [Validators.required]),
+      "startDate":  new FormControl(null, [Validators.required])
+    });
+    
   }
+
+  onSearchStock(){
+    console.log(this.viewStockPrice.value.endDate.toISOString())
+  }
+
+ 
 
 }
