dc68f919900075ed170e817799fc34727d19994e
unknown <suryaprasanth81@gmail.com>
2022-05-14 21:05:49 +0530

    company service completed

--- Frontend/e-stock-market-ui/src/app/service/company-detail.service.spec.ts
+++ Frontend/e-stock-market-ui/src/app/service/company-detail.service.spec.ts
@@ -1,14 +1,90 @@
 import { TestBed } from '@angular/core/testing';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
+import { User } from '../User';
+import { CompanyDetailBean } from '../company-details/companyDetailBean';
 import { CompanyDetailService } from './company-detail.service';
+import { of } from 'rxjs';
 
 describe('CompanyDetailService', () => {
   let service: CompanyDetailService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CompanyDetailService]
+    });
     service = TestBed.inject(CompanyDetailService);
   });
 
- 
+  it('can load instance', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('get login id', () => {
+   service.setLoginId("1");
+   expect(service.getLoginId()).toEqual("1")
+  });
+
+  describe('login', () => {
+    it('makes expected calls', () => {
+      const userStub: User = <any>{};
+      const httpTestingController = TestBed.inject(HttpTestingController);
+      service.login(userStub).subscribe(res => {
+        expect(res).toEqual(userStub);
+      });
+      const req = httpTestingController.expectOne('http://localhost:8083/api/v1.0/market/authentication/authenticate');
+      expect(req.request.method).toEqual('POST');
+      req.flush(userStub);
+      httpTestingController.verify();
+    });
+  });
+
+  describe('registerCompanyDetail', () => {
+    it('makes expected calls', () => {
+      const httpTestingController = TestBed.inject(HttpTestingController);
+      const companyDetailBeanStub: CompanyDetailBean = <any>{};
+      spyOn(service, 'getToken').and.stub();
+              service.registerCompanyDetail(companyDetailBeanStub).subscribe(res => {
+        expect(res).toEqual(companyDetailBeanStub);
+      });
+      expect(service.getToken).toHaveBeenCalled();
+      const req = httpTestingController.expectOne('http://localhost:8083/api/v1.0/market/company/register');
+      expect(req.request.method).toEqual('POST');
+      req.flush(companyDetailBeanStub);
+      httpTestingController.verify();
+    });
+  });
+
+  describe('getAllCompanyDetails', () => {
+    it('makes expected calls', () => {
+      const httpTestingController = TestBed.inject(HttpTestingController);
+      spyOn(service, 'getToken').and.callThrough();
+      service.getAllCompanyDetails().subscribe(res => {
+        // expect(res).toEqual();
+      });
+      expect(service.getToken).toHaveBeenCalled();
+      const req = httpTestingController.expectOne('http://localhost:8083/api/v1.0/market/company/getall');
+      expect(req.request.method).toEqual('GET');
+      // req.flush();
+      httpTestingController.verify();
+    });
+  });
+
+  describe('getAllCompanyDetails', () => {
+    it('makes expected calls', () => {
+      const httpTestingController = TestBed.inject(HttpTestingController);
+      spyOn(service, 'deleteCompanyDetail').and.callThrough();
+     service.deleteCompanyDetail("1").subscribe(res=>{
+
+     })
+      expect(service.deleteCompanyDetail).toHaveBeenCalled();
+      const req = httpTestingController.expectOne('http://localhost:8083/api/v1.0/market/company/delete/1');
+      expect(req.request.method).toEqual('DELETE');
+      // req.flush();
+      httpTestingController.verify();
+    });
+  });
 });
--- Frontend/e-stock-market-ui/src/app/service/stock-price.service.spec.ts
+++ Frontend/e-stock-market-ui/src/app/service/stock-price.service.spec.ts
@@ -1,14 +1,28 @@
 import { TestBed } from '@angular/core/testing';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
+import { StockPriceBean } from 'src/app/stocks/stockPriceBean';
+import { CompanyDetailService } from './company-detail.service';
 import { StockPriceService } from './stock-price.service';
 
 describe('StockPriceService', () => {
   let service: StockPriceService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const companyDetailServiceStub = () => ({ getToken: () => ({}) });
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        StockPriceService,
+        { provide: CompanyDetailService, useFactory: companyDetailServiceStub }
+      ]
+    });
     service = TestBed.inject(StockPriceService);
   });
 
- 
+  it('can load instance', () => {
+    expect(service).toBeTruthy();
+  });
 });
