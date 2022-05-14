d645c415494aff1e1b6a687ea16109984295a2ef
unknown <suryaprasanth81@gmail.com>
2022-05-14 19:47:46 +0530

    component completed

--- Frontend/e-stock-market-ui/src/app/company-details/company-detail-info/company-detail-info.component.spec.ts
+++ Frontend/e-stock-market-ui/src/app/company-details/company-detail-info/company-detail-info.component.spec.ts
@@ -1,23 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { CompanyDetailService } from 'src/app/service/company-detail.service';
 import { CompanyDetailInfoComponent } from './company-detail-info.component';
 
 describe('CompanyDetailInfoComponent', () => {
   let component: CompanyDetailInfoComponent;
   let fixture: ComponentFixture<CompanyDetailInfoComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CompanyDetailInfoComponent ]
-    })
-    .compileComponents();
-  });
-
   beforeEach(() => {
+    const activatedRouteStub = () => ({});
+    const routerStub = () => ({});
+    const companyDetailServiceStub = () => ({});
+    TestBed.configureTestingModule({
+      schemas: [NO_ERRORS_SCHEMA],
+      declarations: [CompanyDetailInfoComponent],
+      providers: [
+        { provide: ActivatedRoute, useFactory: activatedRouteStub },
+        { provide: Router, useFactory: routerStub },
+        { provide: CompanyDetailService, useFactory: companyDetailServiceStub }
+      ]
+    });
     fixture = TestBed.createComponent(CompanyDetailInfoComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
- 
+  it('can load instance', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('can emit', () => {
+    component.deleteCompany(1);
+    component.ngOnInit()
+    console.log("company detail info ",component.companyDetailList)
+    expect(component.companyDetailList).toBeUndefined()
+  });
 });
--- Frontend/e-stock-market-ui/src/app/company-details/view-company-detail/view-company-detail.component.spec.ts
+++ Frontend/e-stock-market-ui/src/app/company-details/view-company-detail/view-company-detail.component.spec.ts
@@ -1,14 +1,28 @@
+import { DatePipe } from '@angular/common';
+import { HttpClientModule, HttpClient, HttpHandler } from '@angular/common/http';
+import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule, FormsModule, NgForm, FormControl, FormGroup, Validators } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { CompanyDetailService } from 'src/app/service/company-detail.service';
 
 import { ViewCompanyDetailComponent } from './view-company-detail.component';
 
+@Component({ selector: 'app-header', template: '' })
+class HeaderComponentStub {
+}
 describe('ViewCompanyDetailComponent', () => {
   let component: ViewCompanyDetailComponent;
   let fixture: ComponentFixture<ViewCompanyDetailComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ViewCompanyDetailComponent ]
+      declarations: [ ViewCompanyDetailComponent , HeaderComponentStub],
+      imports: [RouterTestingModule.withRoutes([
+        { path: 'view-company-detail/:companyCode', component: ViewCompanyDetailComponent }
+      ]), ReactiveFormsModule, FormsModule],
+      providers: [HttpClientModule, HttpClient, HttpHandler, NgForm, DatePipe]
     })
     .compileComponents();
   });
@@ -16,8 +30,45 @@ describe('ViewCompanyDetailComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ViewCompanyDetailComponent);
     component = fixture.componentInstance;
+    component.viewCompanyForm== new FormGroup({
+      "companyCode": new FormControl(null, [Validators.required]),
+      "companyName": new FormControl(null, [Validators.required]),
+      "companyCEO": new FormControl(null, [Validators.required]),
+      "companyTurnOver": new FormControl(null, [Validators.required, Validators.pattern('^[1-9][0-9]*$')]),
+      "companyWebsite": new FormControl(null, [Validators.required]),
+      "stockExchange": new FormControl(null, [Validators.required]),
+      "stockPrice": new FormControl(null, [Validators.required])
+    });
     fixture.detectChanges();
   });
 
 
+  it(`view comapny detail`,()=>{
+    const app=fixture.componentInstance;
+    const companyDetailService=fixture.debugElement.injector.get(CompanyDetailService)
+
+    const companyDetailLists = {
+      "id": "1",
+
+      "companyCode": "1",
+
+      "companyName": "test",
+
+      "companyCEO": "test",
+
+      "companyTurnOver": "9999",
+
+      "companyWebsite": "abc.com",
+
+      "stockExchange": "1",
+
+      "stockPrice": "1"
+    }
+    
+    spyOn(companyDetailService,"viewCompanyDetails").and.callFake(()=>{
+      return of(companyDetailLists)
+    });
+    app.ngOnInit()
+    expect(app.viewCompanyForm.value.companyCode).toEqual(companyDetailLists.companyCode)
+  })
 });
